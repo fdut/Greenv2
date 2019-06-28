a7f0a0b0e0a6555792d5dd3ccda0d7876093feaa
fred <fdutheil@laposte.net>
2019-06-28 15:00:31 +0200

    Update yaml file (Correct syntax error)

--- scripts/local/icloginmvp.sh
+++ scripts/local/icloginmvp.sh
@@ -1,5 +1,5 @@
 ibmcloud login -a cloud.ibm.com -r eu-gb --sso
-ibmcloud ks cluster-config --cluster myfdutcluster
+ibmcloud ks cluster-config --cluster Greenv2
 
 export KUBECONFIG=/Users/fred/.bluemix/plugins/container-service/clusters/Greenv2/kube-config-lon02-Greenv2.yml
 kubectl cluster-info
