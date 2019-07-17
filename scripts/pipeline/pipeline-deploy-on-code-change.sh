b61c690efa8f3145083b20fee94e6a0ab250433e
fred <fdutheil@laposte.net>
2019-07-17 14:51:40 +0200

    add new pipeline scripts

---
+++ scripts/pipeline/pipeline-deploy-on-code-change.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+# uncomment to debug the script
+#set -x
+# copy the script below into your app code repo (e.g. ./scripts/deploy_kubectl.sh) and 'source' it from your pipeline job
+#    source ./scripts/deploy_kubectl.sh
+# alternatively, you can source it from online script:
+#    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/deploy_kubectl.sh")
+# ------------------
+# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/deploy_kubectl.sh
+# Input env variables (can be received via a pipeline environment properties.file.
+echo "IMAGE_NAME=${IMAGE_NAME}"
+echo "IMAGE_TAG=${IMAGE_TAG}"
+echo "REGISTRY_URL=${REGISTRY_URL}"
+echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
+echo "DEPLOYMENT_FILE=${DEPLOYMENT_FILE}"
+echo "DEPLOYMENT_NAME=${DEPLOYMENT_NAME}"
+
+
+
+# List current directory
+echo "Current directory"
+pwd
+
+# View build properties
+if [ -f build.properties ]; then 
+  echo "build.properties:"
+  cat build.properties
+else 
+  echo "build.properties : not found"
+fi 
+
+# Check REDEPLOY variable for redeployment or not of images
+# if $REDEPLOY = "NO" then no redeployment else redeploy or deploy images
+if [ $REDEPLOY = "NO" ];
+then
+  echo "No change REDEPLOY = NO -> Exit"
+  exit
+fi
+
+# also run 'env' command to find all available env variables
+# or learn more about the available environment variables at:
+# https://console.bluemix.net/docs/services/ContinuousDelivery/pipeline_deploy_var.html#deliverypipeline_environment
+
+# Input env variables from pipeline job
+echo "PIPELINE_KUBERNETES_CLUSTER_NAME=${PIPELINE_KUBERNETES_CLUSTER_NAME}"
+if [ -z "${CLUSTER_NAMESPACE}" ]; then CLUSTER_NAMESPACE=default ; fi
+echo "CLUSTER_NAMESPACE=${CLUSTER_NAMESPACE}"
+
+echo "Check existing deployment"
+if kubectl get deployments | grep "${DEPLOYMENT_NAME}" ; then
+
+  echo -e "Rollout to new image "
+  echo -e "kubectl set image deployment/${DEPLOYMENT_NAME} ${DEPLOYMENT_NAME}=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-${IMAGE}:latest"
+  
+  kubectl set image deployment/${DEPLOYMENT_NAME} ${DEPLOYMENT_NAME}="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-${IMAGE}:latest"
+  echo $?
+
+  kubectl rollout status deployment/${DEPLOYMENT_NAME} -n ${CLUSTER_NAMESPACE}
+  echo $?
+
+  kubectl get pods -n ${CLUSTER_NAMESPACE}
+
+else
+  
+  echo -e "Deploy image ${DEPLOYMENT_FILE}"
+  kubectl create -f ${DEPLOYMENT_FILE} -n ${CLUSTER_NAMESPACE}
+
+  echo $?
+
+  kubectl get pods
+
+fi
+
+echo ""
+echo "=========================================================="
+IMAGE_REPOSITORY=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/code-${IMAGE}
+echo -e "CHECKING deployment status of ${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+echo ""
+for ITERATION in {1..30}
+do
+  DATA=$( kubectl get pods --namespace ${CLUSTER_NAMESPACE} -o json )
+  NOT_READY=$( echo $DATA | jq '.items[].status | select(.containerStatuses!=null) | .containerStatuses[] | select(.image=="'"${IMAGE_REPOSITORY}:${IMAGE_TAG}"'") | select(.ready==false) ' )
+  if [[ -z "$NOT_READY" ]]; then
+    echo -e "All pods are ready:"
+    echo $DATA | jq '.items[].status | select(.containerStatuses!=null) | .containerStatuses[] | select(.image=="'"${IMAGE_REPOSITORY}:${IMAGE_TAG}"'") | select(.ready==true) '
+    break # deployment succeeded
+  fi
+  REASON=$(echo $DATA | jq '.items[].status | select(.containerStatuses!=null) | .containerStatuses[] | select(.image=="'"${IMAGE_REPOSITORY}:${IMAGE_TAG}"'") | .state.waiting.reason')
+  echo -e "${ITERATION} : Deployment still pending..."
+  echo -e "NOT_READY:${NOT_READY}"
+  echo -e "REASON: ${REASON}"
+  if [[ ${REASON} == *ErrImagePull* ]] || [[ ${REASON} == *ImagePullBackOff* ]]; then
+    echo "Detected ErrImagePull or ImagePullBackOff failure. "
+    echo "Please check proper authenticating to from cluster to image registry (e.g. image pull secret)"
+    break; # no need to wait longer, error is fatal
+  elif [[ ${REASON} == *CrashLoopBackOff* ]]; then
+    echo "Detected CrashLoopBackOff failure. "
+    echo "Application is unable to start, check the application startup logs"
+    break; # no need to wait longer, error is fatal
+  fi
+  sleep 5
+done
